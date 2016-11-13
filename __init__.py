5e70a2df832d168e0ec391e01ebebede0d103afb
Marlon Falcon Hernandez <falconsoft.3d@gmail.com>
2016-11-13 20:17:51 -0300

    1
    1

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 # For copyright and license notices, see __openerp__.py file in module root
-# directory
+# directory 1
 ##############################################################################
 
 
